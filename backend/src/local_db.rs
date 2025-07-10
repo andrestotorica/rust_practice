3334dff80d216e554aaf1fbf04ee91491ad535ce
Andres Totorica <andrestotorica@gmail.com>
2025-07-10 17:31:41 -0300

    Add tokens of interest stored in DB

--- backend/src/local_db.rs
+++ backend/src/local_db.rs
@@ -1,6 +1,8 @@
 use redis::{Client, Connection, RedisError};
 use std::net::IpAddr;
 
+const TOKENS_SET: &str = "tokens_of_interest";
+
 pub struct LocalDb {
     client: Client,
 }
@@ -28,9 +30,31 @@ impl LocalDb {
         self.client.get_connection()
     }
 
-    pub fn ping(&self) -> Result<String, RedisError> {
+    /// Reads tokens of interest from db. 
+    /// If db is uninitialized it populates provided defaults.
+    ///
+    /// # Arguments
+    ///
+    /// * `defaults` - Default tokens to use if not found in db.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(Vec<String>)` - Tokens of interest from the db.
+    /// * `Err(RedisError)` - Any db error.
+    pub fn read_tokens_or_defaults(&self, defaults: &[&str]) -> Result<Vec<String>, RedisError> {
         let mut con = self.get_connection()?;
-        let pong: String = redis::cmd("PING").query(&mut con)?;
-        Ok(pong)
+        let tokens: Vec<String> = redis::cmd("SMEMBERS")
+            .arg(TOKENS_SET)
+            .query(&mut con)?;
+
+        if tokens.is_empty() {
+            println!("No tokens of interest found in db, populating with defaults");
+            for token in defaults {
+                redis::cmd("SADD").arg(TOKENS_SET).arg(token).execute(&mut con);
+            }
+            Ok(defaults.iter().map(|token| token.to_string()).collect())
+        } else {
+            Ok(tokens)
+        }
     }
 } 
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,11 +2,13 @@ mod env;
 mod local_db;
 use local_db::LocalDb;
 
+const DEFAULT_TOKENS: [&str; 2] = ["UNI", "ZRX"];
+
 fn main() {
     let env_config = env::load_from_env(|key| std::env::var(key));
     
     let local_db = LocalDb::new(env_config.ip, env_config.port).expect("Failed to connect to db");
 
-    let pong = local_db.ping().expect("Failed to ping LocalDb");
-    println!("LocalDb ping successful: {}", pong);
+    let tokens = local_db.read_tokens_or_defaults(&DEFAULT_TOKENS).expect("Failed to read tokens");
+    println!("Tokens: {:?}", tokens);
 }
